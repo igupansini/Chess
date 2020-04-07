d34b13cbe20cbef8aa0d05cb87d733d98a48e45e
IguPansini <pansini@outlook.com.br>
2020-04-07 20:20:26 -0300

    Testando a exceção

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -8,13 +8,22 @@ namespace Chess
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Jogo de Xadrez");
-            Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+            try
+            {
+                Console.WriteLine("Jogo de Xadrez");
+                Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 
-            tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Branca), new Posicao(2, 5));
-            tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(1, 4));
+                tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Branca), new Posicao(2, 5));
+                tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(1, 4));
+                //tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.Branca), new Posicao(1, 10));
+                //tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1, 4));
 
-            Tela.imprimirTabuleiro(tabuleiro);
+                Tela.imprimirTabuleiro(tabuleiro);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
         }
     }
