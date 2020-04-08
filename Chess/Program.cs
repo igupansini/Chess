f2ea0e72d2970e46cb5e5639a96af468902bc6d4
IguPansini <pansini@outlook.com.br>
2020-04-08 20:03:21 -0300

    Modificando a impressao do tabuleiro

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -12,6 +12,21 @@ namespace Chess
             PosicaoXadrez pos = new PosicaoXadrez('a', 1);
             Console.WriteLine(pos);
             Console.WriteLine(pos.toPosicao());
+
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 7));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(3, 4));
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(5, 5));
+
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch(TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
