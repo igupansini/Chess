7ad94b3641dcd224bcaafb7ebe82bc1f86046fb6
IguPansini <pansini@outlook.com.br>
2020-04-08 19:29:52 -0300

    Classe PosicaoXadrez

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -8,23 +8,10 @@ namespace Chess
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Console.WriteLine("Jogo de Xadrez");
-                Tabuleiro tabuleiro = new Tabuleiro(8, 8);
-
-                tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Branca), new Posicao(2, 5));
-                tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(1, 4));
-                //tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.Branca), new Posicao(1, 10));
-                //tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1, 4));
-
-                Tela.imprimirTabuleiro(tabuleiro);
-            }
-            catch (TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
+            Console.WriteLine("Jogo de Xadrez");
+            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.toPosicao());
         }
     }
 }
