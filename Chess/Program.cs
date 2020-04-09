6a60ea3220d060826e6c8202b649a1c48c2be615
IguPansini <pansini@outlook.com.br>
2020-04-09 20:40:26 -0300

    Testes de movimentacao

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -8,12 +8,23 @@ namespace Chess
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\t CHESS\n");
-
             try
             {
                 PartidaXadrez partida = new PartidaXadrez();
-                Tela.imprimirTabuleiro(partida.tab);
+
+                while (!partida.terminada)
+                {
+                    Console.Clear();
+                    Console.WriteLine("\t CHESS\n");
+                    Tela.imprimirTabuleiro(partida.tab);
+                    
+                    Console.WriteLine("\nORIGEM: ");
+                    Posicao origem = Tela.lerPosicao().toPosicao();
+                    Console.WriteLine("\nDESTINO: ");
+                    Posicao destino = Tela.lerPosicao().toPosicao();
+
+                    partida.executaMovimento(origem, destino);
+                }
             }
             catch(TabuleiroException e)
             {
